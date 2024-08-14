21b84a5ea3b1bcd1b308dbe0769bcbfc999b950a
Nikolai Chaunin <chaunnik@cvut.cz>
2024-08-14 12:23:42 +0200

    Initial commit

---
+++ src/main/java/com/example/xent/XentApplication.java
@@ -0,0 +1,13 @@
+package com.example.xent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xent/XentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
