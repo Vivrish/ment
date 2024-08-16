843294838dcf1dabce9093ab3276567888d273d7
Nikolai Chaunin <chaunnik@cvut.cz>
2024-08-16 14:07:36 +0200

    Container database initialisation

--- Microservices/AuthenticationService/src/main/java/com/example/authenticationservice/AuthenticationServiceApplication.java
+++ Microservices/AuthenticationService/src/main/java/com/example/authenticationservice/AuthenticationServiceApplication.java
@@ -1,13 +1,25 @@
 package com.example.authenticationservice;
 
+import com.example.authenticationservice.DTO.UserCredentialsDto;
+import com.example.authenticationservice.service.AuthenticationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(AuthenticationService authenticationService) {
+		return args -> {
+			authenticationService.register(new UserCredentialsDto("Antonio", "password"));
+			System.out.println(authenticationService.getAllUsers().get(0).getName());
+		};
 	}
 
 }
---
+++ Microservices/AuthenticationService/src/main/java/com/example/authenticationservice/domain/User.java
@@ -0,0 +1,49 @@
+package com.example.authenticationservice.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_credentials")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
