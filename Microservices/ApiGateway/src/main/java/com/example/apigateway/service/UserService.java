9354824c7966e01b13c38c5f4ddb9d36834c9d88
Nikolai Chaunin <chaunnik@cvut.cz>
2024-09-01 17:51:29 +0200

    Set up the eureka server in API gateway

---
+++ Microservices/ApiGateway/src/main/java/com/example/apigateway/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.apigateway.service;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+}
