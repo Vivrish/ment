5c834734da83227137fa45d4208ea267d30127bb
Nikolai Chaunin <chaunnik@cvut.cz>
2024-09-02 16:06:04 +0200

    Established communication via kafka

--- Microservices/ApiGateway/src/main/java/com/example/apigateway/service/UserService.java
+++ Microservices/ApiGateway/src/main/java/com/example/apigateway/service/UserService.java
@@ -7,6 +7,7 @@ import com.example.apigateway.feignClients.AuthenticationService;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +15,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserService {
     private final AuthenticationService authenticationService;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void addUser(@NonNull FullUserDto userDto) {
         FullUserCredentialsDto userCredentials = new FullUserCredentialsDto(userDto);
         log.debug("Sending POST request to auth service: {}, {}", userDto.getUsername(), userDto.getPassword());
         authenticationService.register(userCredentials);
-
-
+        kafkaTemplate.send("register", userDto);
     }
 }
