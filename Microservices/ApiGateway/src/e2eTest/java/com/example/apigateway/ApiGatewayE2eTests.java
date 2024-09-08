8a55e69d6d7830bdbfcbf66c07b6e712ab18191c
Nikolai Chaunin <chaunnik@cvut.cz>
2024-09-08 18:31:50 +0200

    Added E2E test task

---
+++ Microservices/ApiGateway/src/e2eTest/java/com/example/apigateway/ApiGatewayE2eTests.java
@@ -0,0 +1,18 @@
+package com.example.apigateway;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("main")
+public class ApiGatewayE2eTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+
+}
