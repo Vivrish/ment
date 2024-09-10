8550773564c7ec56851e25436933db46be10d479
Nikolai Chaunin <chaunnik@cvut.cz>
2024-09-10 17:35:35 +0200

    Updated kafka configuration

--- Microservices/ApiGateway/src/main/java/com/example/apigateway/config/FeignConfig.java
+++ Microservices/ApiGateway/src/main/java/com/example/apigateway/config/FeignConfig.java
@@ -1,16 +1,10 @@
 package com.example.apigateway.config;
 
 import com.example.apigateway.exception.FeignErrorDecoder;
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
 import feign.codec.ErrorDecoder;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.util.Enumeration;
 
 @Configuration
 public class FeignConfig {
