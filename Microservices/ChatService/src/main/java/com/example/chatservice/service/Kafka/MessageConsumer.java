d240b11347e488c2ceaca1dfb58547519d207123
Nikolai Chaunin <chaunnik@cvut.cz>
2024-09-16 18:47:02 +0200

    Added factory specification for Kafka listeners

--- Microservices/ChatService/src/main/java/com/example/chatservice/service/Kafka/MessageConsumer.java
+++ Microservices/ChatService/src/main/java/com/example/chatservice/service/Kafka/MessageConsumer.java
@@ -2,15 +2,13 @@ package com.example.chatservice.service.Kafka;
 
 import com.example.chatservice.service.MessageService;
 import com.example.chatservice.service.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xent.DTO.APIGateway.FullUserDto;
-import com.xent.DTO.ChatService.FullChatUserDto;
 import com.xent.DTO.ChatService.ShortMessageDto;
 import com.xent.DTO.ChatService.ShortChatUserDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +21,8 @@ public class MessageConsumer {
     private final MessageService messageService;
     private final UserService userService;
 
-    @KafkaListener(topicPattern = "topic-room-.*", groupId = "main")
+
+    @KafkaListener(topicPattern = "topic-room-.*", groupId = "main", containerFactory = "kafkaListenerContainerFactoryMessage")
     public void consumeRoomMessage(ShortMessageDto message) {
         log.debug("Consumed message on topic topic-room: {}", message);
         messagingTemplate.convertAndSend(String.format("/topic/room/%s", message.getRoomName()), message.getMessage());
@@ -31,7 +30,7 @@ public class MessageConsumer {
         log.debug("Message consumed: {}", message.getMessage());
     }
 
-    @KafkaListener(topics = "register", groupId = "main")
+    @KafkaListener(topics = "register", groupId = "main", containerFactory = "kafkaListenerContainerFactoryUser")
     public void consumeRegister(FullUserDto fullUserToRegister) {
         log.debug("Consumed message on topic register: {}", fullUserToRegister);
         userService.addUser(new ShortChatUserDto(fullUserToRegister));
