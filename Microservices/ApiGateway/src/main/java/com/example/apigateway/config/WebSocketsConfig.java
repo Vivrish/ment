88e517bd30550845835aa22d70daba7b355e999b
Nikolai Chaunin <chaunnik@cvut.cz>
2024-09-21 19:00:03 +0200

    Manually added the bean

--- Microservices/ApiGateway/src/main/java/com/example/apigateway/config/WebSocketsConfig.java
+++ Microservices/ApiGateway/src/main/java/com/example/apigateway/config/WebSocketsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
 @Configuration
 public class WebSocketsConfig {
@@ -12,5 +13,9 @@ public class WebSocketsConfig {
         return builder.routes()
                 .route("chat_service", r -> r.path("/ws").uri("ws://ChatService:8100/ws"))
                 .build();
+    }
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer() {
+        return ServerCodecConfigurer.create();
     }
 }
