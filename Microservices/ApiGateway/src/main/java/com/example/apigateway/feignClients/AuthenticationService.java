ff8e051a253bc8af20ee3b541dd1dbdd9e96155a
Nikolai Chaunin <chaunnik@cvut.cz>
2024-10-01 16:29:02 +0200

    Deleted eureka from cloud configuration

--- Microservices/ApiGateway/src/main/java/com/example/apigateway/feignClients/AuthenticationService.java
+++ Microservices/ApiGateway/src/main/java/com/example/apigateway/feignClients/AuthenticationService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "AuthenticationService")
+@FeignClient(name = "authservice")
 @Component
 public interface AuthenticationService {
     @PostMapping("/api/v1/register")
